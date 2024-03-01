3e8cf714a6d4069917c9bd3dc2b005245f321784
maverick <manaswiraj195@kgpian.iitkgp.ac.in>
2024-03-01 22:20:31 +0530

    events done

--- frontend/src/app/events/page.tsx
+++ frontend/src/app/events/page.tsx
@@ -2,6 +2,8 @@
 
 import { EventItem } from "@/components/event-item";
 import EventPic from "@/images/animal-after.webp";
+import { DrawerDialogDemo } from "./wrapper";
+
 
 export interface Event {
   name: string;
@@ -14,7 +16,7 @@ const EventList: Event[] = [
     name: "The Weeknd",
     desc: "After Hours",
     thumbnail: "/animal-after.webp",
-  },
+  },  
   {
     name: "The Weeknd",
     desc: "After Hours",
@@ -69,14 +71,12 @@ export default function CarouselPlugin() {
       >
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
-                  </CardContent>
-                </Card>
-              </div>
+            <CarouselItem key={index} className="ring ring-400 ring-opacity-50 focus:ring-2 focus:ring-600 transition-transform transform hover:scale-105">
+              {/* <div className="p-1"> */}
+                {/* <Card className="w-full h-full"> */}
+                  <DrawerDialogDemo/>
+                {/* </Card> */}
+              {/* </div> */}
             </CarouselItem>
           ))}
         </CarouselContent>
